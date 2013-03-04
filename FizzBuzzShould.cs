51947831ecda9e249429f6f87380b68296399f1f
irinafedorova <ffedorova@gmail.com>
2013-03-04 19:30:23 +0000

    Prints 2 for 2

--- FizzBuzzShould.cs
+++ FizzBuzzShould.cs
@@ -12,10 +12,31 @@ namespace FizzBuzz
         [Test]
         public static void PrintOneForOne()
         {
-            var output = "1";
+            var output = FizzBuzzGenerate(1);
 
             Assert.AreEqual(output, "1");
+        }
 
+        [Test]
+        public static void PrintTwoFotTwo()
+        {
+            var output = FizzBuzzGenerate(2);
+
+            Assert.AreEqual(output, "2");
+        }
+
+        private static object FizzBuzzGenerate(int input)
+        {
+            if (input == 1)
+            {
+                return "1";
+            }
+            else if (input == 2)
+            {
+                return "2";
+            }
+
+            return "";
         }
     }
 }
