17662f027558af83711e70dc67732261537f7c94
irinafedorova <ffedorova@gmail.com>
2013-03-04 19:23:55 +0000

    Prints 1 for 1

---
+++ FizzBuzzShould.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public class FizzBuzzShould
+    {
+        [Test]
+        public static void PrintOneForOne()
+        {
+            var output = "1";
+
+            Assert.AreEqual(output, "1");
+
+        }
+    }
+}
